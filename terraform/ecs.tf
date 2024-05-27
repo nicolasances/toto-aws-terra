9fa10c19bc3a1777ecdc63a3bf063dd09d6d272e
nicolasances <nicolas.matteazzi@gmail.com>
2024-05-27 09:22:02 +0200

    IAM Task Role

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -23,3 +23,28 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy_attach
   role = aws_iam_role.toto_ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
+# 1.2. Task Role
+# This is a generic role that ECS Microservices should use in the Task Definition
+resource "aws_iam_role" "toto_ecs_task_role" {
+  name = form("toto-ecs-task-role-%s", var.toto_environment)
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ecs.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_role_secrets_manager" {
+  role = aws_iam_role.toto_ecs_task_role.name
+  policy_arn = "arn:aws:iam::aws:policy/SecretsManagerReadWrite"
+}
