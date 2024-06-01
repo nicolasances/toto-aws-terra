20567f7110df9eda05742f85daf3404b30894521
nicolasances <nicolas.matteazzi@gmail.com>
2024-06-01 12:33:17 +0200

    From ARN to Id

--- terraform/toto-py-service.tf
+++ terraform/toto-py-service.tf
@@ -8,17 +8,17 @@ resource "github_actions_environment_secret" "ecs_subnet_1" {
     repository = "aws-py-service"
     environment = var.toto_environment
     secret_name = "ecs_subnet_1"
-    plaintext_value = aws_subnet.toto_pub_subnet_1.arn
+    plaintext_value = aws_subnet.toto_pub_subnet_1.id
 }
 resource "github_actions_environment_secret" "ecs_subnet_2" {
     repository = "aws-py-service"
     environment = var.toto_environment
     secret_name = "ecs_subnet_2"
-    plaintext_value = aws_subnet.toto_pub_subnet_2.arn
+    plaintext_value = aws_subnet.toto_pub_subnet_2.id
 }
 resource "github_actions_environment_secret" "ecs_security_group" {
     repository = "aws-py-service"
     environment = var.toto_environment
     secret_name = "ecs_security_group"
-    plaintext_value = aws_security_group.toto_open_service.arn
+    plaintext_value = aws_security_group.toto_open_service.id
 }
