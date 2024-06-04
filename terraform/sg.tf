2c3b4b27f66a49e44277042a68473d8390a422a3
nicolasances <nicolas.matteazzi@gmail.com>
2024-06-04 19:48:04 +0200

    Allow 443

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -24,6 +24,13 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tcp_8080" {
   from_port = "8080"
   to_port = "8080"
 }
+resource "aws_vpc_security_group_ingress_rule" "allow_tcp_443" {
+  security_group_id = aws_security_group.toto_open_service.id
+  cidr_ipv4 = "0.0.0.0/0"
+  ip_protocol = "tcp"
+  from_port = "443"
+  to_port = "8080"
+}
 resource "aws_vpc_security_group_egress_rule" "allow_all_outgoing" {
   security_group_id = aws_security_group.toto_open_service.id
   cidr_ipv4 = "0.0.0.0/0"
