a0f96ff103e38a31a95cdde9dbd36c70bd869afe
nicolasances <nicolas.matteazzi@gmail.com>
2024-11-01 09:33:45 +0100

    Removed acl (deprecated)

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,6 +1,5 @@
 resource "aws_s3_bucket" "toto_models_bucket" {
     bucket = format("toto-%s-models.to7o.com", var.toto_environment)
-    acl = "private"
 
     tags = {
         Name = "Toto Dev Models Bucket"
