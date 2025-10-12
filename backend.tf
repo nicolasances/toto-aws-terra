121efe48c1d58acf44bc95f7811beecb098ed94c
nicolasances <nicolas.matteazzi@gmail.com>
2025-10-12 14:57:27 +0200

    Var for Terraform Bucket

--- backend.tf
+++ backend.tf
@@ -7,7 +7,7 @@ terraform {
   }
   required_version = ">= 1.3"
   backend "s3" {
-    bucket      = "nimat-terraform-bucket"    
+    bucket      = "nimat-${var.toto_env}-terraform-bucket"
     key         = "myproject/prod/terraform.tfstate"
     region      = var.aws_region                   
     use_lockfile = true                           # S3 native locking (recommended)
