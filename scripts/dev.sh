19e1e924aed8775f83c34c085304a1f64dece70c
nicolasances <nicolas.matteazzi@gmail.com>
2025-10-17 09:19:35 +0200

    Using TF_VAR

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,6 +1,6 @@
 export TOTO_ENV=dev
 export AWS_REGION=eu-north-1
-export TF_toto_env=dev
-export TF_aws_region=eu-north-1
-export TF_codebuild_role_arn=arn:aws:iam::787222310702:role/TotoCodeBuildRole
-export TF_code_connection_arn=arn:aws:codeconnections:eu-north-1:787222310702:connection/37cdb817-9ca7-40ad-a811-301226c66e09
+export TF_VAR_toto_env=dev
+export TF_VAR_aws_region=eu-north-1
+export TF_VAR_codebuild_role_arn=arn:aws:iam::787222310702:role/TotoCodeBuildRole
+export TF_VAR_code_connection_arn=arn:aws:codeconnections:eu-north-1:787222310702:connection/37cdb817-9ca7-40ad-a811-301226c66e09
