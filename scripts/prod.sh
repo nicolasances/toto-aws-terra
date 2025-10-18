e590d2f2fae04fdf8f54ecc9bca910c431616aae
nicolasances <nicolas.matteazzi@gmail.com>
2025-10-18 15:56:59 +0200

    Added certificate

--- scripts/prod.sh
+++ scripts/prod.sh
@@ -4,4 +4,4 @@ export TF_VAR_toto_env=prod
 export TF_VAR_aws_region=eu-north-1
 export TF_VAR_code_connection_arn=arn:aws:codeconnections:eu-north-1:787222310702:connection/37cdb817-9ca7-40ad-a811-301226c66e09
 export TF_VAR_code_connection_hash=37cdb817-9ca7-40ad-a811-301226c66e09
-export TF_VAR_alb_certificate_arn=
+export TF_VAR_alb_certificate_arn=arn:aws:acm:eu-north-1:787222310702:certificate/d31e4c73-ea5d-49f2-835c-e465a2ea2f9b
