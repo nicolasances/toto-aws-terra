fe672b41d57d6787643453f2c93b604ebf291c2a
nicolasances <nicolas.matteazzi@gmail.com>
2025-12-27 14:23:43 +0100

    Creation of Secret for Topic ARN

--- sns.tf
+++ sns.tf
@@ -19,14 +19,23 @@ resource "aws_sns_topic_subscription" "tome_topics_tome_ms_agents_subscription" 
 
 
 ########################################################
-# 2. Topic names Secrets
+# 2. Topic-related Secrets
 ########################################################
 resource "aws_secretsmanager_secret" "topic_name_tometopics_secret" {
   name        = format("%s/%s", var.toto_env, "tome_topics_topic_name")
   description = "Secret for the name of the topic for events on Tome Topic"
 }
-
 resource "aws_secretsmanager_secret_version" "topic_name_tometopics_secret_version" {
   secret_id     = aws_secretsmanager_secret.topic_name_tometopics_secret.id
   secret_string = aws_sns_topic.tome_topics_topic.name
 }
+# Tome Topics - Topic ARN
+resource "aws_secretsmanager_secret" "topic_id_tometopics_secret" {
+  name        = format("%s/%s", var.toto_env, "tome_topics_topic_id")
+  description = "Secret for the ARN of the topic for events on Tome Topic"
+}
+resource "aws_secretsmanager_secret_version" "topic_id_tometopics_secret_version" {
+  secret_id     = aws_secretsmanager_secret.topic_id_tometopics_secret.id
+  secret_string = aws_sns_topic.tome_topics_topic.arn
+}
+
